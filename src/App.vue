d2c18e5060744e81d7360240136855b22cd5fe8b
Samir Sadek <samirsadek2009@gmail.com>
2023-12-15 20:22:41 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Hello From Samir Sadek" />
+      <Home />
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>Home Component {{ "20 + 20".length }}</h1>
+  <h2>Email: {{ email }}</h2>
+  <h2>mobile: {{ mobile }}</h2>
+  <h2>Name: {{ getName("samir sadek") }}</h2>
+
+  <h2>---------Event Click---------</h2>
+
+  <p>{{ count }}</p>
+  <button v-on:click="test()">Increment</button>
+  <button v-on:click="test2()">Decrement</button>
+
+  <h2>----Two Way Binding--------</h2>
+  <input type="text" v-model="count" />
+  <h2>{{ count }}</h2>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      email: "samirsadek@1009.com",
+      mobile: 1020,
+      count: 0,
+      getName: function (a) {
+        return a;
+      },
+    };
+  },
+  methods: {
+    getData() {
+      return {
+        name: "samir",
+        email: this.email,
+      };
+    },
+    test() {
+      this.count = this.count + 1;
+    },
+    test2() {
+      this.count = this.count - 1;
+    },
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: brown;
+}
+</style>
